3ec0988b1f362eda4eb627dcf1652feed3a3b10c
ceci-arangio <ceciliaarangio@gmail.com>
2023-12-05 18:06:28 +0100

    testeo

--- ferrimas2/src/App.jsx
+++ ferrimas2/src/App.jsx
@@ -7,6 +7,7 @@ import PoliticaCookies from './Pages/PoliticaCookies/PoliticaCookies';
 import AvisoLegal from './Pages/AvisoLegal/AvisoLegal';
 import Contacto from './components/Contacto/Contacto';
 import Tienda from './Pages/Tienda/Tienda';
+import Testeo1 from './components/Testeos/Testeo1';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Route path='/politica-de-cookies' element={<PoliticaCookies/>}/>
         <Route path='/aviso-legal' element={<AvisoLegal/>}/>
         <Route path='/tienda' element={<Tienda/>}/>
+        <Route path='/testeo' element={<Testeo1/>}/>
       </Routes>
     </Router>
     </div>
