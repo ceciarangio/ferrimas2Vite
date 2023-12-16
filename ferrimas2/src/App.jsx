33954b3c7d610c010f2b27c8994cf09a8b552488
ceci-arangio <ceciliaarangio@gmail.com>
2023-12-16 11:33:21 +0100

    pages

--- ferrimas2/src/App.jsx
+++ ferrimas2/src/App.jsx
@@ -15,11 +15,8 @@ import Seguridad from './Pages/Seguridad/Seguridad';
 import Carteleria from './Pages/Carteleria/Carteleria';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n'; // Importa tu configuración de i18n
-<<<<<<< HEAD
-import MenuMaquinaMobile from './components/MenuMaquinaMobile/MenuMaquinaMobile';
-=======
 
->>>>>>> 5dfc0075ca72d17b46bed120f927d4b12dd8411a
+import MenuMaquinaMobile from './components/MenuMaquinaMobile/MenuMaquinaMobile';
 
 function App() {
 
@@ -48,10 +45,6 @@ function App() {
     </Router>
     </div>
     </I18nextProvider>
-<<<<<<< HEAD
-=======
-    
->>>>>>> 5dfc0075ca72d17b46bed120f927d4b12dd8411a
     </>
   )
 }
