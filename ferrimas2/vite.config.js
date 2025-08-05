a2b788c1dd176476f383d9888d4084e09d6c3bf2
ceci-arangio <ceciliaarangio@gmail.com>
2025-08-05 13:42:11 +0200

    fix: set base path for Vite build

--- ferrimas2/vite.config.js
+++ ferrimas2/vite.config.js
@@ -1,7 +1,22 @@
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+//export default defineConfig({
+//  plugins: [react()],
+//})
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './', // 👈 importante para rutas relativas
+  build: {
+    outDir: 'dist' // 👈 por si acaso, aunque es el valor por defecto
+  },
+  plugins: [react()]
 })
+
